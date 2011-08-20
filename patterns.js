735cb20dfae15c0982573c6a49e55dca7e191f9e
Christopher Groskopf <staringmonkey@gmail.com>
2011-08-19 20:44:13 -0500

    Stripping code.

--- patterns.js
+++ patterns.js
@@ -1,43 +1,45 @@
-$(function init() {
-  var treemap = new $jit.TM.Squarified({
-    //where to inject the visualization
-    injectInto: 'infovis',
-    //parent box title heights
-    titleHeight: 15,
-    //enable animations
-    animate: true,
-    //box offsets
-    offset: 1,
-    duration: 1000,
-    //Enable tips
-    Tips: {
-      enable: true,
-      //add positioning offsets
-      offsetX: 20,
-      offsetY: 20,
-      //implement the onShow method to
-      //add content to the tooltip when a node
-      //is hovered
-      onShow: function(tip, node, isLeaf, domElement) {
-        tip.innerHTML = "<div class=\"tip-title\">" + node.name + "</div><div class=\"tip-text\">";
-      }  
-    },
-    //Add the name of the node in the correponding label
-    //This method is called once, on label creation.
-    onCreateLabel: function(domElement, node){
-        domElement.innerHTML = node.name;
-        var style = domElement.style;
-        style.display = '';
-        style.border = '1px solid transparent';
-        domElement.onmouseover = function() {
-          style.border = '1px solid #9FD4FF';
-        };
-        domElement.onmouseout = function() {
-          style.border = '1px solid transparent';
-        };
-    }
-  });
+var treemap;
 
-  treemap.loadJSON(DATA);
-  treemap.refresh();
+function show_tooltip(tip, node, isLeaf, domElement) {
+    tip.innerHTML = "<div class=\"tip-title\">" + node.name + "</div><div class=\"tip-text\">";
+}
+
+function create_label(domElement, node) {
+    domElement.innerHTML = node.name;
+
+    var style = domElement.style;
+    style.display = '';
+    style.border = '1px solid transparent';
+    
+    domElement.onmouseover = function() {
+        style.border = '1px solid #9FD4FF';
+    };
+    
+    domElement.onmouseout = function() {
+        style.border = '1px solid transparent';
+    };
+}
+
+$(function init() {
+    treemap = new $jit.TM.Squarified({
+        injectInto: 'infovis',
+        titleHeight: 30,
+        animate: true,
+        offset: 1,
+        duration: 1000,
+        Tips: {
+            enable: true,
+            offsetX: 20,
+            offsetY: 20,
+            onShow: show_tooltip
+        },
+        onCreateLabel: create_label 
+    });
+
+    treemap.loadJSON(DATA);
+    treemap.refresh();
+
+    $(window).resize(function() {
+        treemap.refresh();
+    });
 });
